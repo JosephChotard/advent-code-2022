40f6ff7972bfd650cb194c3afb5088ec409b4adc
Joseph Chotard <jchotard@palantir.com>
2022-12-06 23:27:26 +0300

    d6 p2

--- 06/main.py
+++ 06/main.py
@@ -4,7 +4,7 @@ def read_input():
     return input
 
 
-def a(input):
+def find_marker(input, length):
     last_chars = []
     for i, char in enumerate(input):
         if char in last_chars:
@@ -13,11 +13,14 @@ def a(input):
             last_chars += char
         else:
             last_chars += char
-            if len(last_chars) == 4:
+            if len(last_chars) == length:
                 return i+1
 
+def a(input):
+    return find_marker(input, 4)
+
 def b(input):
-    return 0
+    return find_marker(input, 14)
 
 def asserts():
     assert(a('bvwbjplbgvbhsrlpgdmjqwftvncz') == 5)
@@ -25,6 +28,11 @@ def asserts():
     assert(a('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 10)
     assert(a('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') == 11)
     assert(a('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 7)
+    assert(b('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 19)
+    assert(b('bvwbjplbgvbhsrlpgdmjqwftvncz') == 23)
+    assert(b('nppdvjthqldpwncqszvftbrmjlhg') == 23)
+    assert(b('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 29)
+    assert(b('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') == 26)
 
 if __name__ == '__main__':
     input = read_input()
